87240d72cf26d34a89bb150ef274e08a12419775
maxblessed91 <vadams3@yandex.ru>
2020-10-03 04:50:42 +0400

    init: start typical-arrays-problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  // let sorted = array.sort();
+  // array.sort((a, b) => a - b);
+  // array.splice(0, 1);
+  // if (array === [ ]) return 0;
+  // let result = array[0];
+  // for (let i = 0; i < array.length; i++) {
+  if (array && array.length !== 0) {
+    let result = Math.min(...array);
+    return result;
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  // let result = [];
+  // for (let i=0; i < array.length; i++){
+  if (array && array.length !== 0) {
+    let result = Math.max(...array);
+    return result;
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length !== 0) {
+    let result = array.reduce((acc, item) => acc + item, 0);
+    let total = result / array.length;
+    return total;
+    // let result = array.reduce((acc, item) => (acc + item) / item, 1);
+    // let total = array.reduce();
+    // return total;
+  }
+  else {
+    return 0;
+  }
 }
